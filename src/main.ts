0aa1adcec28ac24ebed9c397baebac0fb99a6458
bonyoungkoo <gby1202@gmail.com>
2025-06-28 02:34:02 +0900

    refactor: 포트 변경 및 프론트엔드 CORS 허용

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  await app.listen(process.env.PORT ?? 3002);
+  app.enableCors({
+    origin: 'http://localhost:3002', // 프론트엔드 주소
+    credentials: true,
+  });
+  await app.listen(process.env.PORT ?? 3003);
 }
 
 void bootstrap();
